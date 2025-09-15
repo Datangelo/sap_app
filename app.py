8e86bbeff26882a78da392bf725cccc3c5fa2415
Datangelo <angelo.democrito@tdsynnex.com>
2025-09-15 18:08:43 +0200

    erasing # from blobstorage stuff

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ load_dotenv()
 app = Flask(__name__)
 
 # Configuration via environment variables
-#STORAGE_ACCOUNT_URL = os.environ.get("STORAGE_ACCOUNT_URL")
-#CONTAINER_NAME      = os.environ.get("CONTAINER_NAME")
+STORAGE_ACCOUNT_URL = os.environ.get("STORAGE_ACCOUNT_URL")
+CONTAINER_NAME      = os.environ.get("CONTAINER_NAME")
 
 # Initialize blob service client
-#blob_service_client = BlobServiceClient(
-#    account_url=STORAGE_ACCOUNT_URL,
-#    credential=DefaultAzureCredential()
-#)
+blob_service_client = BlobServiceClient(
+    account_url=STORAGE_ACCOUNT_URL,
+    credential=DefaultAzureCredential()
+)
 
 
 @app.route('/')
@@ -379,6 +379,7 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))  # fallback to 8000 for local testing
     app.run(host='0.0.0.0', port=port)
     
+
 
 
 
